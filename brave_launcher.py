29a543387ef248ee01d3e9a0d9a9b59571c0cd6f
Nicolas Orefice <123935503+Testeh69@users.noreply.github.com>
2024-07-15 21:38:02 +0200

    Update brave_launcher.py

--- brave_launcher.py
+++ brave_launcher.py
@@ -4,12 +4,12 @@ import subprocess
 
 
 
-brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+brave_path : str = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 if not os.path.exists(brave_path):
     raise FileNotFoundError(f"Brave executable not found at {brave_path}")
 
 # URL à ouvrir
-url = "http://localhost:8080/"
+url: str = "http://localhost:8080/"
 
 # Ouvrir Brave avec l'URL spécifiée
 subprocess.Popen([brave_path, url])
